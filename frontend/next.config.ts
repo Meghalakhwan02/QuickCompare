fa8792d25771932cdb1a1c5bb2d7042ecf719dab
Your Name <MEGHA@MEGHA-CC.costacloud.in>
2026-01-14 15:29:56 +0530

    updated next version

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    // Ensuring the root is set to the current directory (frontend)
-    // to avoid confusion with the lockfile on the Desktop.
-    root: __dirname,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
